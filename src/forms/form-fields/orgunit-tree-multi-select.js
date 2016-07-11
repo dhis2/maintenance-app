9a6d589940e9c9e37dcc56d22378c13cedb5b7f7
Nicolay Ramm <nicolay@dhis2.org>
2016-07-11 16:07:51 +0200

    Add group/level/all org unit selection

--- src/forms/form-fields/orgunit-tree-multi-select.js
+++ src/forms/form-fields/orgunit-tree-multi-select.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import OrgunitTree from 'd2-ui/lib/org-unit-tree/OrgUnitTreeMultipleRoots.component';
+import OrgUnitTree from 'd2-ui/lib/org-unit-tree/OrgUnitTreeMultipleRoots.component';
+import OrgUnitSelectByLevel from 'd2-ui/lib/org-unit-select/OrgUnitSelectByLevel.component';
+import OrgUnitSelectByGroup from 'd2-ui/lib/org-unit-select/OrgUnitSelectByGroup.component';
+import OrgUnitSelectAll from 'd2-ui/lib/org-unit-select/OrgUnitSelectAll.component';
 import TextField from 'material-ui/lib/text-field';
 import Action from 'd2-ui/lib/action/Action';
 import { Observable } from 'rx';
@@ -20,13 +23,33 @@ export default class OrganisationUnitTreeMultiSelect extends React.Component {
         };
 
         this._searchOrganisationUnits = Action.create('searchOrganisationUnits');
+
+        this._handleClick = this._handleClick.bind(this);
+        this._setSelection = this._setSelection.bind(this);
     }
 
     componentDidMount() {
-        this.context.d2.currentUser.getOrganisationUnits()
-            .then(ou => ou.toArray())
-            .then(rootOrgUnits => {
-                rootOrgUnits
+        const d2 = this.context.d2;
+
+        Promise.all([
+            d2.currentUser.getOrganisationUnits(),
+            d2.models.organisationUnitLevels.list({
+                paging: false,
+                fields: 'id,level,displayName',
+                order: 'level:asc',
+            }),
+            d2.models.organisationUnitGroups.list({
+                paging: false,
+                fields: 'id,displayName',
+            }),
+        ])
+            .then(([
+                orgUnits,
+                levels,
+                groups,
+            ]) => {
+                const rootOrgUnits = orgUnits
+                    .toArray()
                     .filter(ou => (new RegExp(`${this.state.searchValue}`)).test(ou.displayName));
 
                 this.setState({
@@ -35,6 +58,8 @@ export default class OrganisationUnitTreeMultiSelect extends React.Component {
                     selectedOrgUnits: this.props.value
                         .toArray()
                         .map(ou => ou.id),
+                    orgUnitGroups: groups,
+                    orgUnitLevels: levels,
                 });
             });
 
@@ -64,18 +89,20 @@ export default class OrganisationUnitTreeMultiSelect extends React.Component {
 
     renderRoots() {
         if (this.state.rootOrgUnits.length) {
-            return this.state.rootOrgUnits.map(rootOu => {
-                return (
-                    <OrgunitTree
-                        key={rootOu.id}
-                        selected={this.state.selectedOrgUnits}
-                        root={rootOu}
-                        onClick={this._handleClick.bind(this)}
-                        emitModel
-                        initiallyExpanded={[rootOu.id]}
-                    />
-                );
-            });
+            return (
+                <div style={{ maxHeight: 350, maxWidth: 480, overflow: 'auto' }}>
+                    {this.state.rootOrgUnits.map(rootOu => (
+                        <OrgUnitTree
+                            key={rootOu.id}
+                            selected={this.state.selectedOrgUnits}
+                            root={rootOu}
+                            onClick={this._handleClick}
+                            emitModel
+                            initiallyExpanded={[rootOu.id]}
+                        />
+                    ))}
+                </div>
+            );
         }
 
         return (
@@ -88,17 +115,73 @@ export default class OrganisationUnitTreeMultiSelect extends React.Component {
             return (<div>this.context.d2.i18n.getTranslation('determining_your_root_orgunits')</div>);
         }
 
+        const controlStyles = {
+            float: 'right',
+            // position: 'absolute',
+            // top: 64, right: 24,
+            width: 475,
+            zIndex: 1,
+            background: 'white',
+        };
+
         return (
-            <div>
+            <div style={{ position: 'relative', minWidth: 800 }}>
                 <TextField
                     onChange={(event) => this._searchOrganisationUnits(event.target.value)}
                     floatingLabelText={this.context.d2.i18n.getTranslation('filter_organisation_units_by_name')}
                     fullWidth
                 />
                 <div className="organisation-unit-tree__selected">{this.state.selectedOrgUnits.length} {this.context.d2.i18n.getTranslation('organisation_units_selected')}</div>
+                {this.state.orgUnitGroups && this.state.orgUnitLevels && (
+                    <div style={controlStyles}>
+                        <OrgUnitSelectByLevel
+                            levels={this.state.orgUnitLevels}
+                            selected={this.state.selectedOrgUnits}
+                            onUpdateSelection={this._setSelection}
+                        />
+                        <OrgUnitSelectByGroup
+                            groups={this.state.orgUnitGroups}
+                            selected={this.state.selectedOrgUnits}
+                            onUpdateSelection={this._setSelection}
+                        />
+                        <div style={{ marginTop: 16 }}>
+                            <OrgUnitSelectAll
+                                selected={this.state.selectedOrgUnits}
+                                onUpdateSelection={this._setSelection}
+                            />
+                        </div>
+                    </div>
+                )}
                 {this.renderRoots()}
             </div>
         );
+    }
+
+    _setSelection(selectedOrgUnits) {
+        const d2 = this.context.d2;
+        const modelOrgUnits = this.props.model.organisationUnits;
+        const assigned = modelOrgUnits.toArray().map(ou => ou.id);
+
+        const additions = selectedOrgUnits
+        // Filter out already assigned ids
+            .filter(id => assigned.indexOf(id) === -1)
+            // Add the rest
+            .map(id => d2.models.organisationUnits.create({ id }));
+
+        const deletions = assigned
+        // Filter out ids that should be left in
+            .filter(id => selectedOrgUnits.indexOf(id) === -1)
+            // Add the rest
+            .map(id => d2.models.organisationUnits.create({ id }));
+
+        additions.forEach(ou => {
+            modelOrgUnits.add(ou);
+        });
+        deletions.forEach(ou => {
+            modelOrgUnits.remove(ou);
+        });
+
+        this.setState({ selectedOrgUnits });
     }
 
     _handleClick(event, orgUnit) {
@@ -126,8 +209,11 @@ export default class OrganisationUnitTreeMultiSelect extends React.Component {
     }
 }
 OrganisationUnitTreeMultiSelect.contextTypes = {
-    d2: React.PropTypes.object,
+    d2: React.PropTypes.object.isRequired,
 };
+OrganisationUnitTreeMultiSelect.propTypes = {
+    value: React.PropTypes.object,
+}
 OrganisationUnitTreeMultiSelect.defaultProps = {
     value: [],
 };
