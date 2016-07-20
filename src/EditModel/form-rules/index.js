9eed585dcd6b3d1b3b3c3041f26b175bd75c8977
Mark Polak <mark@thedutchies.com>
2016-07-20 16:11:55 +0200

    Disable aggregationOperator for certain valueTypes
    Resolves #57

--- src/EditModel/form-rules/index.js
+++ src/EditModel/form-rules/index.js
@@ -1,10 +1,12 @@
 import noop from 'd2-utilizr/lib/noop';
 import log from 'loglevel';
 import fieldRules from '../../config/field-rules';
+import isArray from 'd2-utilizr/lib/isArray';
 
 const whenOperatorMap = new Map([
     ['EQUALS', equalsOperator],
     ['HAS_VALUE', hasValueOperator],
+    ['ONEOF', oneOfOperator]
 ]);
 
 const operationsMap = new Map([
@@ -51,20 +53,32 @@ function equalsOperator(left, right) {
     return left === right;
 }
 
-function ruleRunner(rule, fieldConfig, model) {
-    const whenFieldName = rule.when.field ? rule.when.field : rule.field;
-    const operatorFn = getWhenOperator(rule.when.operator);
-    const whenValue = rule.when.value;
+function oneOfOperator(value, list) {
+    return list.indexOf(value) >= 0;
+}
 
+function ruleRunner({whenFieldName, operatorFn, whenValue}, fieldConfig, model) {
     return operatorFn(model[whenFieldName], whenValue)
+}
+
+function rulesRunner(rules, rule, modelToEdit, fieldConfigs) {
+    return rules.map(whenRule => {
+        log.debug(`For ${rule.field} run the rule where when ${whenRule.field || rule.field} ${getWhenOperator(whenRule.operator).name} ${whenRule.value || ''} then run`);
+        const fieldConfigForRule = fieldConfigs.find(fieldConfig => fieldConfig.name === (whenRule.field || rule.field));
+
+        const whenFieldName = whenRule.field ? whenRule.field : rule.field;
+        const operatorFn = getWhenOperator(whenRule.operator);
+        const whenValue = whenRule.value;
+
+        return ruleRunner({whenFieldName, operatorFn, whenValue}, fieldConfigForRule, modelToEdit);
+    });
 }
 
 export function applyRulesToFieldConfigs(rules, fieldConfigs, modelToEdit) {
     rules
-        .map((rule) => {
-            log.debug(`For ${rule.field} run the rule where when ${rule.when.field || rule.field} ${getWhenOperator(rule.when.operator).name} ${rule.when.value || ''} then run`);
-            const fieldConfigForRule = fieldConfigs.find(fieldConfig => fieldConfig.name === (rule.when.field || rule.field));
-            const rulePassed = ruleRunner(rule, fieldConfigForRule, modelToEdit);
+        .forEach((rule) => {
+            const rules = isArray(rule.when) ? rule.when : [rule.when];
+            const rulePassed = rulesRunner(rules, rule, modelToEdit, fieldConfigs).some(result => result === true);
 
             log.debug(`And the result is`, rulePassed);
 
--- src/config/field-rules.js
+++ src/config/field-rules.js
@@ -1,3 +1,29 @@
+/**
+ * Rules for the form fields.
+ * If multiple `when` objects are specified these are evaluated as an OR.
+ * The following would result check if either of the statements return true
+ * ```
+    when: [{
+        field: 'valueType',
+        operator: 'ONEOF',
+        value: [
+            'TEXT',
+            'LONG_TEXT',
+            'LETTER',
+            'PHONE_NUMBER',
+            'EMAIL',
+            'TRACKER_ASSOCIATE',
+            'USERNAME',
+            'FILE_RESOURCE',
+            'COORDINATE',
+        ]
+        }, {
+            field: 'domainType',
+            operator: 'EQUALS',
+            value: 'TRACKER',
+        }],
+ * ```
+ */
 export default new Map([['dataElement',
     [
         {
@@ -12,18 +38,6 @@ export default new Map([['dataElement',
                 propName: 'disabled',
                 thenValue: true,
                 elseValue: false,
-            }, {
-                field: 'aggregationType',
-                type: 'SET_PROP',
-                propName: 'disabled',
-                thenValue: true,
-                elseValue: false,
-            }, {
-                field: 'aggregationType',
-                type: 'CHANGE_VALUE',
-                setValue: (model, fieldConfig) => {
-                    fieldConfig.value = model[fieldConfig.property] = 'NONE';
-                },
             }],
         },
         {
@@ -46,10 +60,48 @@ export default new Map([['dataElement',
                         // collide and is not very transparent. Especially the fact that the new value needs to be set
                         // on both the model and the fieldConfig is not very clear.
                         // It would probably make sense to run the model modification rules before.
-                        fieldConfig.value = model[fieldConfig.property] = model.optionSet.valueType;
+                        fieldConfig.value = model[fieldConfig.name] = model.optionSet.valueType;
                     },
                 }
             ],
+        },
+        {
+            field: 'aggregationType',
+            when: [{
+                field: 'valueType',
+                operator: 'ONEOF',
+                value: [
+                    'TEXT',
+                    'LONG_TEXT',
+                    'LETTER',
+                    'PHONE_NUMBER',
+                    'EMAIL',
+                    'TRACKER_ASSOCIATE',
+                    'USERNAME',
+                    'FILE_RESOURCE',
+                    'COORDINATE',
+                ]
+            }, {
+                field: 'domainType',
+                operator: 'EQUALS',
+                value: 'TRACKER',
+            }],
+            operations: [
+                {
+                    field: 'aggregationType',
+                    type: 'SET_PROP',
+                    propName: 'disabled',
+                    thenValue: true,
+                    elseValue: false,
+                },
+                {
+                    field: 'aggregationType',
+                    type: 'CHANGE_VALUE',
+                    setValue: (model, fieldConfig) => {
+                        fieldConfig.value = model[fieldConfig.name] = 'NONE';
+                    },
+                }
+            ]
         }
     ],
 ], ['attribute', [
