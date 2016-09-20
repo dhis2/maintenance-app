2433426342c6d12b3242f1bdc13ebf3e342a7ef7
Nicolay Ramm <nicolay@dhis2.org>
2016-09-20 18:27:58 +0200

    SubFieldWrap styling experiment

--- src/config/field-overrides/helpers/SubFieldWrap.js
+++ src/config/field-overrides/helpers/SubFieldWrap.js
@@ -5,12 +5,13 @@ const styles = {
         transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
         boxSizing: 'content-box',
         borderRadius: '2px',
-        padding: '1rem',
         width: '100%',
         backgroundColor: 'rgb(250, 250, 250)',
-        marginLeft: '-5rem',
-        paddingLeft: '7rem',
-        paddingRight: '3rem',
+        paddingLeft: '1rem',
+        paddingBottom: '0.5rem',
+        marginLeft: '-0.5rem',
+        marginBottom: '1rem',
+        boxShadow: 'rgba(0, 0, 0, 0.15) 0px 2px 5px -1px inset',
     },
 };
 
