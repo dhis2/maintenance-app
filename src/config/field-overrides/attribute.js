68fbb7f75d4f792775af449afe62f2d30f53634b
Mark Polak <mark@thedutchies.com>
2017-03-03 22:59:05 +0100

    Dynamically render attribute checkboxes
    Render attribute checkbox for each schema that supports it
    
    DHIS2-927

---
+++ src/config/field-overrides/attribute.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AttributeAssignment from './attribute/AttributeAssignment';
+
+export default new Map([
+    ['dataElementAttribute', {
+        component: AttributeAssignment,
+    }],
+]);
