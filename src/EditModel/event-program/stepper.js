75588e5d26d0b66cd2ea6bc76f257b977a760875
Mark Polak <mark@thedutchies.com>
2017-03-21 16:09:52 +0100

    Add comments for stepper creators

--- src/EditModel/event-program/stepper.js
+++ src/EditModel/event-program/stepper.js
@@ -5,6 +5,24 @@ import Step from 'material-ui/Stepper/Step';
 import Translate from 'd2-ui/lib/i18n/Translate.component';
 import log from 'loglevel';
 
+/**
+ * Create a Stepper component from a configuration object.
+ *
+ * @param {Array} stepperConfig Config array that defines the steps that should be shown.
+ *
+ * @returns {ReactComponent} A React component that will render the steps as defined by the `stepperConfig`
+ *
+ * @example
+ * ```
+ * const steps = [
+ *      { key: 'first', name: 'First step!' },
+ *      { key: 'last', name: 'My last step!' },
+ * ];
+ * const MyStepperComponent = createStepperFromConfig(steps)
+ *
+ * // <MyStepperComponent activeStep={activeStep.key} />
+ * ```
+ */
 export const createStepperFromConfig = (stepperConfig) => ({ activeStep, stepperClicked }) => (
     <Stepper linear={false}>
         {stepperConfig.map(step => (
@@ -15,6 +33,28 @@ export const createStepperFromConfig = (stepperConfig) => ({ activeStep, stepper
     </Stepper>
 );
 
+/**
+ * Create a StepperContent component from a configuration object. The StepperContent component renders the content that
+ * belongs to the current `activeStep`. When the config does not specify a `component` property for the current active
+ * step a warning is logged and `null` is rendered.
+ * The `component` is passed all the props passed to the StepperContent.
+ *
+ * @param {Array} stepperConfig Config array that defines the steps and the components that should be rendered for the
+ * currently active step.
+ *
+ * @returns {ReactComponent} A React component that will render the `component` property of the currently active step.
+ *
+ * @example
+ * ```
+ * const steps = [
+ *      { key: 'first', name: 'First step!', component: () => (<div>First</div>) },
+ *      { key: 'last', name: 'My last step!', component: () => (<div>First</div>) },
+ * ];
+ * const MyStepperComponent = createStepperFromConfig(steps)
+ *
+ * // <MyStepperComponent activeStep="first" /> // Will render <div>First</div>
+ * ```
+ */
 export const createStepperContentFromConfig = (stepperConfig) => ({ activeStep, ...props }) => {
     const step = stepperConfig.find(stepConfig => stepConfig.key === activeStep);
 
