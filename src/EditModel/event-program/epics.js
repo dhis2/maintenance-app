563f4b1ba3f999ef1370ee6f28331ce18204b169
Mark Polak <mark@thedutchies.com>
2017-05-18 16:09:17 +0200

    Load new programs with an empty organisationUnit collection (D227TEST-12, DHIS2-10)

--- src/EditModel/event-program/epics.js
+++ src/EditModel/event-program/epics.js
@@ -37,7 +37,8 @@ function loadEventProgramMetadataByProgramId(programId) {
                 id: programUid,
                 programStages: [
                     { id: programStageUid }
-                ]
+                ],
+                organisationUnits: [],
             }],
             programStages: [{
                 id: programStageUid,
