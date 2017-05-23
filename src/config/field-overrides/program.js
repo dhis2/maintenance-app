efaabc79322b5a37276c123191b7faeebd9b0f36
Mark Polak <mark@thedutchies.com>
2017-05-23 14:12:01 +0200

    Only show catCombos of type ATTRIBUTE for program (DHIS2-1701)

---
+++ src/config/field-overrides/program.js
@@ -0,0 +1,7 @@
+export default new Map([
+    ['categoryCombo', {
+        fieldOptions: {
+            queryParamFilter: ['dataDimensionType:eq:ATTRIBUTE', 'name:eq:default'],
+        },
+    }],
+])
