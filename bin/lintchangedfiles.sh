952e54f7757e749b55a17c7cb9d7cb889719c0f2
Jen Jones Arnesen <jarnesen@baosystems.com>
2017-09-04 09:59:52 +0200

    update script to check all files changed compared to master

---
+++ bin/lintchangedfiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CMT=`git merge-base origin/master HEAD`
+FILE_STRING=`git diff --name-only $CMT | grep \.js | grep -v \.json;`
+
+declare -a LIST=( $FILE_STRING )
+
+for f in "${LIST[@]}"
+do
+    ./node_modules/.bin/eslint $f
+done
