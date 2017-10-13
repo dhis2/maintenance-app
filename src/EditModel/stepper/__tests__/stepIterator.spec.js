f902ccfd5339a71e5855ecd020186860f2db0a8b
Jen Jones Arnesen <jjarnesen@gmail.com>
2017-10-13 14:27:25 +0200

    Chore/use jest framework (#361)
    As agreed upon, switching to jest for unit tests

--- src/EditModel/stepper/__tests__/stepIterator.spec.js
+++ src/EditModel/stepper/__tests__/stepIterator.spec.js
@@ -20,34 +20,34 @@ const bacalaoSteps = [
 
 describe('Step iterator', () => {
     describe('next', () => {
-        it('should return the next item when current item is not last', () => {
+        test('should return the next item when current item is not last', () => {
             const actualStep = next(bacalaoSteps, COOK_INGREDIENTS);
-            expect(actualStep).to.equal(SERVE_BACALAO);
+            expect(actualStep).toBe(SERVE_BACALAO);
         });
 
-        it('should return the same item when current item is last', () => {
+        test('should return the same item when current item is last', () => {
             const actualStep = next(bacalaoSteps, SERVE_BACALAO);
-            expect(actualStep).to.equal(SERVE_BACALAO);
+            expect(actualStep).toBe(SERVE_BACALAO);
         });
     });
 
     describe('previous', () => {
-        it('should return the previous item when current item is not first', () => {
+        test('should return the previous item when current item is not first', () => {
             const actualStep = previous(bacalaoSteps, COOK_INGREDIENTS);
-            expect(actualStep).to.equal(GET_INGREDIENTS);
+            expect(actualStep).toBe(GET_INGREDIENTS);
         });
 
 
-        it('should return the same item when current item is first', () => {
+        test('should return the same item when current item is first', () => {
             const actualStep = previous(bacalaoSteps, GET_INGREDIENTS);
-            expect(actualStep).to.equal(GET_INGREDIENTS);
+            expect(actualStep).toBe(GET_INGREDIENTS);
         });
     });
 
     describe('first', () => {
-        it('should return the first item', () => {
+        test('should return the first item', () => {
             const actualStep = first(bacalaoSteps);
-            expect(actualStep).to.equal(GET_INGREDIENTS);
+            expect(actualStep).toBe(GET_INGREDIENTS);
         });
     });
 });
