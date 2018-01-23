252ef81214f22dcb742bc7f0930a5527aa7b0e1c
Birk Johansson <birk@dhis2.org>
2018-01-23 11:27:26 +0100

    ProgramStageList to class, add sharing context-option

--- src/EditModel/event-program/tracker-program/program-stages/ProgramStageList.js
+++ src/EditModel/event-program/tracker-program/program-stages/ProgramStageList.js
@@ -2,40 +2,30 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import DataTable from 'd2-ui/lib/data-table/DataTable.component';
 import programStore$ from '../../eventProgramStore';
-import mapProps from 'recompose/mapProps';
-import compose from 'recompose/compose';
-import mapPropsStream from 'recompose/mapPropsStream';
+import {
+    compose,
+    lifecycle,
+    withHandlers,
+    withState,
+    withStateHandlers,
+} from 'recompose';
 import { get, noop, first, getOr, __, sortBy } from 'lodash/fp';
 import {
     getTableColumnsForType,
     getFilterFieldsForType,
     getFiltersForType,
 } from '../../../../config/maintenance-models';
-import withState from 'recompose/withState';
 import FloatingActionButton from 'material-ui/FloatingActionButton/FloatingActionButton';
 import FontIcon from 'material-ui/FontIcon/FontIcon';
 import { addQuery } from '../../../../router-utils';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { editProgramStage, addProgramStage, deleteProgramStage } from './actions';
-
-const enhance = compose(
-    connect(null, dispatch =>
-        bindActionCreators(
-            {
-                handleEditProgramStage: model => editProgramStage(model.id),
-                handleNewProgramStage: () => addProgramStage(),
-                handleDeleteProgramStage: model => deleteProgramStage(model.id)
-            },
-            dispatch
-        )
-    ),
-    withState(
-        'tableColumns',
-        'setTableColumns',
-        getTableColumnsForType('programStage')
-    )
-);
+import {
+    editProgramStage,
+    addProgramStage,
+    deleteProgramStage,
+} from './actions';
+import SharingDialog from 'd2-ui/lib/sharing/SharingDialog.component';
 
 const FAB = props => {
     const cssStyles = {
@@ -57,38 +47,97 @@ const FAB = props => {
 };
 
 function isContextActionAllowed(model, action) {
-   return true;
+    return true;
 }
 
-const ProgramStageList = props => {
-    const contextActions = {
-        edit: props.handleEditProgramStage,
-        delete: props.handleDeleteProgramStage,
-        translate: () => {},
+class ProgramStageList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sharing: {
+                modelType: 'programStage',
+                id: null,
+            },
+            translate: {},
+            tableColumns: getTableColumnsForType('programStage'),
+        };
+    }
+
+    openSharing = model => {
+        this.setState({
+            ...this.state,
+            sharing: {
+                ...this.state.sharing,
+                id: model.id
+            }
+        })
     };
 
-    const contextMenuIcons = {
-        edit: 'edit',
-        move_up: 'arrow_upward',
-        move_down: 'arrow_downward',
+    closeSharing = () => {
+        this.setState({
+            ...this.state,
+            sharing: {
+                ...this.state.sharing,
+                id: null,
+            },
+        });
     };
 
-    return (
-        <div>
-            <DataTable
-                rows={props.programStages}
-                columns={props.tableColumns}
-                primaryAction={props.handleEditProgramStage}
-                contextMenuActions={contextActions}
-                contextMenuIcons={contextMenuIcons}
-                isContextActionAllowed={isContextActionAllowed}
-            />
-            <FAB router={props} {...props} />
-        </div>
-    );
-};
+    renderSharing = () => {
+        return this.state.sharing.id
+            ? <SharingDialog
+                  id={this.state.sharing.id}
+                  type={this.state.sharing.modelType}
+                  open={!!this.state.sharing.id}
+                  onRequestClose={this.closeSharing}
+                  bodyStyle={{ minHeight: '400px' }}
+              />
+            : null;
+    };
+
+    render() {
+        const contextActions = {
+            edit: this.props.handleEditProgramStage,
+            share: this.openSharing,
+            delete: this.props.handleDeleteProgramStage,
+            translate: () => {},
+        };
+
+        const contextMenuIcons = {
+            edit: 'edit',
+            share: 'share',
+            move_up: 'arrow_upward',
+            move_down: 'arrow_downward',
+        };
+
+        return (
+            <div>
+                <DataTable
+                    rows={this.props.programStages}
+                    columns={this.props.tableColumns}
+                    primaryAction={this.props.handleEditProgramStage}
+                    contextMenuActions={contextActions}
+                    contextMenuIcons={contextMenuIcons}
+                    isContextActionAllowed={isContextActionAllowed}
+                />
+                <FAB {...this.props} />
+                {this.renderSharing()}
+            </div>
+        );
+    }
+}
 
 ProgramStageList.propTypes = {
     programStages: PropTypes.array,
 };
-export default enhance(ProgramStageList);
+export default connect(null, dispatch =>
+    bindActionCreators(
+        {
+            handleEditProgramStage: model => editProgramStage(model.id),
+            handleNewProgramStage: () => addProgramStage(),
+            handleDeleteProgramStage: model => deleteProgramStage(model.id),
+        },
+        dispatch
+    )
+)(ProgramStageList);
