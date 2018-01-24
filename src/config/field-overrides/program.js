cb09da55daf9ef920446abdaede583cc14752fec
Birk Johansson <birk@dhis2.org>
2018-01-24 16:53:02 +0100

    Add custom realtionshipFromA field. Fix null-bug in drop-down-async-getter

--- src/config/field-overrides/program.js
+++ src/config/field-overrides/program.js
@@ -1,15 +1,54 @@
 import PeriodTypeDropDown from '../../forms/form-fields/period-type-drop-down';
 //import AttributeSelector from '../../'
-export default new Map([
-    ['categoryCombo', {
-        fieldOptions: {
-            queryParamFilter: ['dataDimensionType:eq:ATTRIBUTE', 'name:eq:default'],
+import { SELECT } from '../../forms/fields';
+import DropDownAsyncGetter from '../../forms/form-fields/drop-down-async-getter';
+
+async function getRelationshipTypes(model, d2) {
+    const relationship = await d2.models.relationshipTypes.get(
+        model.relationshipType.id
+    );
+    const relationhipOptions = [
+        {
+            text: relationship.aIsToB,
+            value: true,
         },
-    }],
-    ['expiryPeriodType', {
-        component: PeriodTypeDropDown,
-    }],
-  /*  ['programAttributes', {
-        component:
-    }] */
+        {
+            text: relationship.bIsToA,
+            value: false,
+        },
+    ];
+
+    return relationhipOptions;
+}
+
+export default new Map([
+    [
+        'categoryCombo',
+        {
+            fieldOptions: {
+                queryParamFilter: [
+                    'dataDimensionType:eq:ATTRIBUTE',
+                    'name:eq:default',
+                ],
+            },
+        },
+    ],
+    [
+        'expiryPeriodType',
+        {
+            component: PeriodTypeDropDown,
+        },
+    ],
+    // Translate realtionShipFromA to a drop-down consisting of the relationships
+    [
+        'relationshipFromA',
+        {
+            component: DropDownAsyncGetter,
+            persisted: true,
+            fieldOptions: {
+                getter: getRelationshipTypes,
+                useValueDotId: false,
+            },
+        },
+    ],
 ]);
