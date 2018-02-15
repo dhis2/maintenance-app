78739f0ca707ce18ba1a57fc71ed43d89f982b2a
Birk Johansson <birk@dhis2.org>
2018-02-15 17:31:11 +0100

    Fix delete programStage

--- src/EditModel/event-program/tracker-program/program-stages/contextActions.js
+++ src/EditModel/event-program/tracker-program/program-stages/contextActions.js
@@ -5,7 +5,7 @@ import camelCaseToUnderscores from 'd2-utilizr/lib/camelCaseToUnderscores';
 import { getInstance } from 'd2/lib/d2';
 import log from 'loglevel';
 import { set } from 'lodash/fp';
-import { deleteProgramStageFromState } from "../epics";
+import { deleteProgramStageFromState } from '../epics';
 import { Observable } from 'rxjs';
 
 export async function deleteProgramStageWithSnackbar(model) {
@@ -20,45 +20,33 @@ export async function deleteProgramStageWithSnackbar(model) {
             model.name,
         ].join(' '),
         action: 'confirm',
-        //TODO we cant delete from the API if the item is not already saved to the server!
-        // e.g. when creating a new stage then deleting
-        onActionTouchTap: () => {
-            model
-                .delete()
-                .then(() => {
-                    deleteProgramStageFromState(model.id);
-                    snackActions.show({
-                        message: `${model.displayName} ${d2.i18n.getTranslation(
-                            'was_deleted'
-                        )}`,
-                    });
 
-                    // Fire the afterDeleteHook
-                    afterDeleteHook$.next({
-                        model,
-                        modelType: model.modelDefinition.name,
-                    });
-                })
-                .catch(response => {
-                    snackActions.show({
-                        message: response.message
-                            ? response.message
-                            : `${model.name} ${d2.i18n.getTranslation(
-                                  'was_not_deleted'
-                              )}`,
-                        action: 'ok',
-                    });
-                });
+        onActionTouchTap: () => {
+            deleteProgramStageFromState(model.id);
+            snackActions.show({
+                message: `${model.displayName} ${d2.i18n.getTranslation(
+                    'was_deleted'
+                )}`,
+            });
+
+            // Fire the afterDeleteHook
+            afterDeleteHook$.next({
+                model,
+                modelType: model.modelDefinition.name,
+            });
         },
     });
 }
 
-
 export function translationSaved() {
     snackActions.show({ message: 'translation_saved', translate: true });
-};
+}
 
 export function translationError(errorMessage) {
     log.error(errorMessage);
-    snackActions.show({ message: 'translation_save_error', action: 'ok', translate: true });
-};
+    snackActions.show({
+        message: 'translation_save_error',
+        action: 'ok',
+        translate: true,
+    });
+}
