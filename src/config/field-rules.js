43b766129b91db750b449e3d381158ece723ddfc
Birk Johansson <birk@dhis2.org>
2018-02-21 20:12:11 +0100

    Fix valueType being cleared when editing optionSet
    Fixes DHIS2-1446

--- src/config/field-rules.js
+++ src/config/field-rules.js
@@ -24,8 +24,28 @@
         }],
  * ```
  */
-export default new Map([['dataElement',
-    [
+
+// TODO: This function does a mutable modification. It is more efficient this way however it might
+// collide and is not very transparent. Especially the fact that the new value needs to be set
+// on both the model and the fieldConfig is not very clear.
+// It would probably make sense to run the model modification rules before sending the values to
+// the FormBuilder.
+function setOptionSetValue(model, fieldConfig) {
+    // Do not not change the valueType when there is no optionSet or when there is no valueType
+    // for the optionSet (which can occur during the initial run of the rules)
+    if (model.optionSet && model.optionSet.valueType) {
+        // Update the fieldConfig to contain the correct value
+        fieldConfig.value = model.optionSet.valueType;
+
+        // Update the model only when the value is not the same as the current
+        if (model[fieldConfig.name] !== model.optionSet.valueType) {
+            model[fieldConfig.name] = model.optionSet.valueType;
+        }
+    }
+}
+
+export default new Map([
+    ['dataElement', [
         {
             field: 'domainType',
             when: {
@@ -55,26 +75,8 @@ export default new Map([['dataElement',
                 },
                 {
                     type: 'CHANGE_VALUE',
-                    // TODO: This function does a mutable modification. It is more efficient this way however it might
-                    // collide and is not very transparent. Especially the fact that the new value needs to be set
-                    // on both the model and the fieldConfig is not very clear.
-                    // It would probably make sense to run the model modification rules before sending the values to
-                    // the FormBuilder.
-                    setValue: (model, fieldConfig) => {
-                        // Do not not change the valueType when there is no optionSet or when there is no valueType
-                        // for the optionSet (which can occur during the initial run of the rules)
-                        if (model.optionSet && model.optionSet.valueType) {
-                            // Update the fieldConfig to contain the correct value
-                            fieldConfig.value = model.optionSet.valueType;
-
-                            // Update the model only when the value is not the same as the current
-                            if (model[fieldConfig.name] !== model.optionSet.valueType) {
-                                model[fieldConfig.name] = model.optionSet.valueType;
-                            }
-                        }
-                    },
-                }
-            ],
+                    setValue: setOptionSetValue
+            }],
         },
         {
             field: 'aggregationType',
@@ -127,9 +129,7 @@ export default new Map([['dataElement',
             },
             {
                 type: 'CHANGE_VALUE',
-                setValue: (model, fieldConfig) => {
-                    fieldConfig.value = model[fieldConfig.name] = model.optionSet.valueType;
-                },
+                setValue: setOptionSetValue
             }
         ]
     }
@@ -180,9 +180,7 @@ export default new Map([['dataElement',
             },
             {
                 type: 'CHANGE_VALUE',
-                setValue: (model, fieldConfig) => {
-                    fieldConfig.value = model[fieldConfig.name] = model.optionSet.valueType;
-                },
+                setValue: setOptionSetValue
             }
         ]
     }
