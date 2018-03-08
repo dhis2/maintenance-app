e2322956b4c357dccc998eb5ba81d174b9ff398b
John Melin <jemelin@student.matnat.uio.no>
2018-03-08 12:36:10 +0100

    fixed onActionTouchTap undefined on action ok (#432)

--- src/Snackbar/SnackbarContainer.component.js
+++ src/Snackbar/SnackbarContainer.component.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Snackbar from 'material-ui/Snackbar/Snackbar';
 import { get } from 'lodash/fp';
-import { hideSnackBarMessage } from './actions';
 import addD2Context from 'd2-ui/lib/component-helpers/addD2Context';
+import { hideSnackBarMessage } from './actions';
 
 const snackBarMessageSelector = get('snackBar.message');
 const snackBarActionHandlerSelector = get('snackBar.onActionTouchTap');
@@ -14,29 +14,30 @@ const snackBarTranslate = get('snackBar.translate');
 
 let SnackBar = (props, { d2 }) => (
     <Snackbar
-        style={{maxWidth: 'auto', zIndex: 5}}
-        bodyStyle={{maxWidth: 'auto'}}
+        style={{ maxWidth: 'auto', zIndex: 5 }}
+        bodyStyle={{ maxWidth: 'auto' }}
         message={props.translate ? d2.i18n.getTranslation(props.message) : props.message}
         action={props.action}
         autoHideDuration={props.autoHideDuration}
         open={!!props.message}
-        onActionTouchTap={props.actionHandler}
+        // if no onActionTouchTap is provided, action will default to close
+        onActionTouchTap={props.actionHandler ? props.actionHandler : props.onRequestClose}
         onRequestClose={props.onRequestClose}
-/>);
+    />);
 SnackBar = addD2Context(SnackBar);
 
 const mapStateToProps = state => (
     {
-        message: snackBarMessageSelector(state) || '',
-        action: snackBarActionTextSelector(state) || '',
-        autoHideDuration: snackBarActionAutoHideSelector(state) || 0,
-        actionHandler: snackBarActionHandlerSelector(state) || null,
-        translate: snackBarTranslate(state) || false,
+        message: snackBarMessageSelector(state),
+        action: snackBarActionTextSelector(state),
+        autoHideDuration: snackBarActionAutoHideSelector(state),
+        actionHandler: snackBarActionHandlerSelector(state),
+        translate: snackBarTranslate(state),
     }
 );
 
 const mapDispatchToProps = dispatch => ({
-    onRequestClose: (...args) => dispatch(hideSnackBarMessage(...args)),
+    onRequestClose: (...args) => dispatch(hideSnackBarMessage({ ...args })),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SnackBar);
@@ -46,5 +47,13 @@ SnackBar.propTypes = {
     action: PropTypes.string,
     actionHandler: PropTypes.func,
     autoHideDuration: PropTypes.number,
-    onRequestClose: PropTypes.func,
+    translate: PropTypes.bool,
+    onRequestClose: PropTypes.func.isRequired,
+};
+
+SnackBar.defaultProps = {
+    action: '',
+    translate: false,
+    actionHandler: null,
+    autoHideDuration: 0,
 };
--- src/Snackbar/reducers.js
+++ src/Snackbar/reducers.js
@@ -11,29 +11,29 @@ const initialSnackState = {
 
 export default function snackReducer(state = initialSnackState, action) {
     switch (action.type) {
-        case 'NOTIFY_USER':
-        case SNACK_BAR_MESSAGE_SHOW: {
-            const {
-                message,
-                action: snackBarAction,
-                autoHideDuration,
-                onActionTouchTap,
-                translate,
-            } = action.payload;
+    case 'NOTIFY_USER':
+    case SNACK_BAR_MESSAGE_SHOW: {
+        const {
+            message,
+            action: snackBarAction,
+            autoHideDuration,
+            onActionTouchTap,
+            translate,
+        } = action.payload;
 
-            return {
-                show: true,
-                message,
-                action: snackBarAction,
-                autoHideDuration,
-                onActionTouchTap,
-                translate,
-            };
-        }
-        case SNACK_BAR_MESSAGE_HIDE:
-            return initialSnackState;
-        default:
-            break;
+        return {
+            show: true,
+            message,
+            action: snackBarAction,
+            autoHideDuration,
+            onActionTouchTap,
+            translate,
+        };
+    }
+    case SNACK_BAR_MESSAGE_HIDE:
+        return initialSnackState;
+    default:
+        break;
     }
 
     return state;
