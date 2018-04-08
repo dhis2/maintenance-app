f87b0dadba5e6ae38825f2109638fd86f24f3870
john-melin <jemelin@student.matnat.uio.no>
2018-04-08 16:43:19 +0200

    moved filter to its own file!

--- src/List/search-and-filters/SearchAndFilters.component.js
+++ src/List/search-and-filters/SearchAndFilters.component.js
@@ -1,16 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { path } from 'lodash/fp';
 import log from 'loglevel';
 
 import Pagination from 'd2-ui/lib/pagination/Pagination.component';
 import SearchBox from './SearchBox.component';
-import Dropdown from '../../forms/form-fields/drop-down';
-import DropdownAsync from '../../forms/form-fields/drop-down-async';
+import Filter from './Filter.component';
 
 import { getFilterFieldsForType } from '../../config/maintenance-models';
-import getConstantDisplayNameOrOld from '../helpers/getConstantDisplayNameOrOld';
 import listActions from '../list.actions';
 
 const styles = {
@@ -18,9 +15,6 @@ const styles = {
         display: 'inline-block',
         marginRight: 16,
         width: 256,
-    },
-    filtersDropDownAsync: {
-        display: 'relative',
     },
     searchAndFilterWrap: {
         clear: 'both',
@@ -32,22 +26,6 @@ const styles = {
 };
 
 class SearchAndFilters extends Component {
-    getTranslation = key => this.context.d2.i18n.getTranslation(key);
-
-    getReferenceType = filterField => (
-        this.context.d2.models.hasOwnProperty(filterField)
-            ? filterField
-            : `${this.props.modelType}.${filterField}`
-    )
-
-    getConstants = (modelDefinition, filterField) => (
-        modelDefinition.modelProperties[filterField].constants.map(c =>
-            ({
-                text: getConstantDisplayNameOrOld(this.props.modelType, filterField, c),
-                value: c,
-            }))
-    )
-
     searchListByName = (searchObserver) => {
         const searchListByNameDisposable = searchObserver
             .subscribe((value) => {
@@ -58,62 +36,32 @@ class SearchAndFilters extends Component {
                     searchString: value,
                 }).subscribe(() => {}, error => log.error(error));
             });
+
         this.props.setSearchListDisposable(searchListByNameDisposable);
     }
 
-    makeFilterSetter = filterField => (e) => {
-        this.props.setIsLoadingState();
-
-        listActions.setFilterValue({
-            filterField,
-            filterValue: e.target.value,
-            modelType: this.props.modelType,
-        });
-    };
-
-    isConstantField = (modelDefinition, filterField) =>
-        path(`${filterField}.constants`, modelDefinition.modelProperties)
-
-    renderFilters = (filterField) => {
-        const modelDefinition = this.context.d2.models[this.props.modelType];
-
-        return (
-            <div key={filterField} style={styles.box}>
-                {this.isConstantField(modelDefinition, filterField)
-                    ? (<Dropdown
-                        labelText={this.getTranslation(filterField)}
-                        options={this.getConstants(modelDefinition, filterField)}
-                        onChange={this.makeFilterSetter(filterField)}
-                        value={this.props.filters ? this.props.filters[filterField] : null}
-                        translateOptions={filterField !== 'periodType'}
-                    />)
-                    : (<DropdownAsync
-                        labelText={this.getTranslation(filterField)}
-                        referenceType={this.getReferenceType(filterField)}
-                        onChange={this.makeFilterSetter(filterField)}
-                        value={this.props.filters ? this.props.filters[filterField] : null}
-                        quickAddLink={false}
-                        preventAutoDefault
-                        style={styles.filterDrowDownAsync}
-                        limit={1}
-                        top={-15}
-                    />)
-                }
-            </div>
-        );
-    }
-
-
     render() {
+        const Filters = getFilterFieldsForType(this.props.modelType)
+            .map(filterField =>
+                (<div key={filterField} style={styles.box}>
+                    <Filter
+                        filters={this.props.filters}
+                        filterField={filterField}
+                        setIsLoadingState={this.props.setIsLoadingState}
+                        modelType={this.props.modelType}
+                    />
+                </div>),
+            );
+
         return (
             <div style={styles.searchAndFilterWrap}>
                 <div style={styles.topPagination}>
                     <Pagination {...this.props.paginationProps} />
                 </div>
+
                 <div>
                     <SearchBox searchObserverHandler={this.searchListByName} />
-                    {getFilterFieldsForType(this.props.modelType)
-                        .map(filterField => this.renderFilters(filterField))}
+                    {Filters}
                 </div>
             </div>
         );
@@ -130,13 +78,6 @@ SearchAndFilters.propTypes = {
         PropTypes.object,
     ]),
 };
-
-SearchAndFilters.defaultProps = {
-    filters: undefined,
-};
-
-SearchAndFilters.contextTypes = {
-    d2: PropTypes.object.isRequired,
-};
+SearchAndFilters.defaultProps = { filters: undefined };
 
 export default SearchAndFilters;
