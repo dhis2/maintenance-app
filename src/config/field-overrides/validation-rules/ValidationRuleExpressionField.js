ebfc85c00ccb84be34821e1d162d4f8838db6569
John Melin <jemelin@student.matnat.uio.no>
2018-04-25 11:31:23 +0200

    DHIS2-3332 Fixed error with validation rule expressions only being saved sometimes. (#450)
    * DHIS2-3332 added discard function on expression cancel, set save button to disable on invalid expression
    
    * Update ValidationRuleExpressionDialog.js

--- src/config/field-overrides/validation-rules/ValidationRuleExpressionField.js
+++ src/config/field-overrides/validation-rules/ValidationRuleExpressionField.js
@@ -1,8 +1,11 @@
-import React, { PropTypes } from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withState, withProps, compose, onlyUpdateForKeys } from 'recompose';
+
+import RaisedButton from 'material-ui/RaisedButton';
 import Row from 'd2-ui/lib/layout/Row.component';
 import Translate from 'd2-ui/lib/i18n/Translate.component';
+
 import ValidationRuleExpressionDialog from './ValidationRuleExpressionDialog';
 
 const styles = {
@@ -18,24 +21,26 @@ const styles = {
     },
 };
 
-function ValidationRuleExpressionField({ value = {}, onButtonClick, open, close, onChange, buttonLabel }) {
+function ValidationRuleExpressionField({ value, onButtonClick, open, close, onChange, buttonLabel }) {
+    const onSave = (newValue) => {
+        onChange({ target: { value: newValue } });
+        close();
+    };
+
     return (
         <div style={styles.field}>
             <Row>
-                <RaisedButton style={styles.expressionButton} onClick={onButtonClick}><Translate>{buttonLabel}</Translate></RaisedButton>
-                <div style={styles.expressionDescription}>{value.description}</div>
+                <RaisedButton style={styles.expressionButton} onClick={onButtonClick}>
+                    <Translate>{buttonLabel}</Translate>
+                </RaisedButton>
+                <div style={styles.expressionDescription}>
+                    {value.description}
+                </div>
             </Row>
             <ValidationRuleExpressionDialog
                 open={open}
                 close={close}
-                save={(value) => {
-                    onChange({
-                        target: {
-                            value,
-                        },
-                    });
-                    close();
-                }}
+                save={onSave}
                 value={value}
                 buttonLabel={buttonLabel}
             />
@@ -49,7 +54,21 @@ const enhance = compose(
     withProps(({ toggleOpen }) => ({
         onButtonClick: () => toggleOpen(true),
         close: () => toggleOpen(false),
-    }))
+    })),
 );
+
+ValidationRuleExpressionField.propTypes = {
+    value: PropTypes.object,
+    onButtonClick: PropTypes.func.isRequired,
+    close: PropTypes.func.isRequired,
+    open: PropTypes.bool,
+    onChange: PropTypes.func.isRequired,
+    buttonLabel: PropTypes.string,
+};
+ValidationRuleExpressionField.defaultProps = {
+    value: {},
+    open: false,
+    buttonLabel: '',
+};
 
 export default enhance(ValidationRuleExpressionField);
