d076e1cfa85a8639f89cebb3cd0713f887b658fb
john-melin <jemelin@student.matnat.uio.no>
2018-05-14 16:49:43 +0200

    DHIS2-3588 save button not enabled onchange

--- src/EditModel/EditModelForm.component.js
+++ src/EditModel/EditModelForm.component.js
@@ -117,7 +117,7 @@ export default React.createClass({
             isLoading: true,
             formState: {
                 validating: false,
-                valid: false,
+                valid: true,
                 pristine: true,
             },
             activeStep: 0,
