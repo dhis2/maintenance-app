983e6043b9f0ee1277a06802a6c761cb5eccb5ee
john-melin <jemelin@student.matnat.uio.no>
2018-05-22 15:16:44 +0200

    removed select color string

--- src/forms/form-fields/color-picker.js
+++ src/forms/form-fields/color-picker.js
@@ -29,7 +29,7 @@ export default function ColorPickerField(props) {
         <div>
             <div style={styles.field} >
                 <div style={styles.colorPickerLabel}> Color </div>
-                <ColorPicker color={props.value || 'Select color'} {...props} onChange={transformChange} />
+                <ColorPicker color={props.value} {...props} onChange={transformChange} />
             </div>
             <Divider />
         </div>
