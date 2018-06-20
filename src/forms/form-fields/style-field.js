8b56b760c3ea94e4894ca4e91933aa42fa851fe8
Hendrik de Graaf <hendrik@dhis2.org>
2018-06-20 13:05:08 +0200

    Switched to using "Swatches" colour picker with 64 colour palette as specified in the issue

--- src/forms/form-fields/style-field.js
+++ src/forms/form-fields/style-field.js
@@ -6,8 +6,25 @@ import LoadableComponent from '../../utils/LoadableComponent';
 import FieldWrapper from './helpers/FieldWrapper';
 
 const LoadablePicker = LoadableComponent({
-    loader: () => import('react-color/lib/components/twitter/Twitter'),
+    loader: () => import('react-color/lib/components/swatches/Swatches'),
 });
+
+const colors = [
+    [ '#ffcdd2', '#e57373', '#d32f2f', '#f06292', '#c2185b', '#880e4f', '#f50057' ], 
+    [ '#e1bee7', '#ba68c8', '#8e24aa', '#aa00ff', '#7e57c2', '#4527a0', '#7c4dff', '#6200ea' ],
+    [ '#c5cae9', '#7986cb', '#3949ab', '#304ffe' ],
+    [ '#e3f2fd', '#64b5f6', '#1976d2', '#0288d1' ],
+    [ '#40c4ff', '#00b0ff', '#80deea' ],
+    [ '#00acc1', '#00838f', '#006064' ], 
+    [ '#e0f2f1', '#80cbc4', '#00695c', '#64ffda' ],
+    [ '#c8e6c9', '#66bb6a', '#2e7d32', '#1b5e20' ],
+    [ '#00e676', '#aed581', '#689f38', '#33691e' ],
+    [ '#76ff03', '#64dd17', '#cddc39', '#9e9d24', '#827717' ],
+    [ '#fff9c4', '#fbc02d', '#f57f17', '#ffff00', '#ffcc80', '#ffccbc', '#ffab91' ],
+    [ '#bcaaa4', '#8d6e63', '#4e342e' ],
+    [ '#fafafa', '#bdbdbd', '#757575', '#424242' ],
+    [ '#cfd8dc', '#b0bec5', '#607d8b', '#37474f' ]
+];
 
 const styles = {
     wrapper: {
@@ -144,6 +161,8 @@ export default class StyleFields extends Component {
                         />
                         <div style={mergedStyles.picker}>
                             <LoadablePicker
+                                className="swatches"
+                                colors={colors}
                                 color={this.state.style.color}
                                 onChangeComplete={this.handleColorChange}
                             />
