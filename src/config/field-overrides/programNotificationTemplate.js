de2bb8a18d74004ccc5449ac1bb303857b8be5b5
Hendrik de Graaf <hendrik@dhis2.org>
2018-07-11 16:12:56 +0200

    Fix notification template variables for ProgramStage notifications - Programs show: a constant set of variables + TEA - Program stages show: another constant set of variable + TEA + DE

--- src/config/field-overrides/programNotificationTemplate.js
+++ src/config/field-overrides/programNotificationTemplate.js
@@ -35,8 +35,8 @@ const toVariableType = name => ['V', name];
 const toAttributeType = name => ['A', name]; // Used for program attributes
 const toDataElementType = name => ['#', name];
 
-const dataElementsTypeMap = dataElements =>
-    map(toDataElementType, dataElements);
+const variablesTypeMap = variables => map(toVariableType, variables);
+const dataElementsTypeMap = dataElements => map(toDataElementType, dataElements);
 const attributesToTypeMap = attributes => map(toAttributeType, attributes);
 
 const boundOnUpdate = dispatch =>
@@ -51,20 +51,21 @@ const boundOnUpdate = dispatch =>
 const NotificationSubjectAndMessageTemplateFields = compose(
     connect(undefined, boundOnUpdate),
     withProps(({ dataElements, attributes, isProgram }) => {
-        // TODO: FIX here once desired logic has been clarified:
-        // https://jira.dhis2.org/browse/DHIS2-4252?focusedCommentId=17771&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-17771
-        let constantVariables = PROGRAM_STAGE_VARIABLES;
-        let variables = dataElementsTypeMap(dataElements);
-
         if (isProgram) {
-            constantVariables = PROGRAM_VARIABLES;
-            variables = attributesToTypeMap(attributes);
+            return {
+                variableTypes: [
+                    ...variablesTypeMap(PROGRAM_VARIABLES),
+                    ...attributesToTypeMap(attributes),
+                ],
+            };
         }
 
         return {
-            variableTypes: map(toVariableType, constantVariables).concat(
-                variables,
-            ),
+            variableTypes: [
+                ...variablesTypeMap(PROGRAM_STAGE_VARIABLES),
+                ...attributesToTypeMap(attributes),
+                ...dataElementsTypeMap(dataElements),
+            ],
         };
     }),
 )(SubjectAndMessageTemplateFields);
