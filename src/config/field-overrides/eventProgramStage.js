49c67b7693b1331c625be62eade48abfdbe47920
Birk Johansson <birk@dhis2.org>
2018-11-28 01:14:39 +0100

    fix: Remove unused featuretype options

---
+++ src/config/field-overrides/eventProgramStage.js
@@ -0,0 +1,12 @@
+import { featureTypeOverride } from './program';
+
+export default new Map([
+    [
+        'featureType',
+        {
+            fieldOptions: {
+                options: featureTypeOverride,
+            },
+        },
+    ],
+]);
