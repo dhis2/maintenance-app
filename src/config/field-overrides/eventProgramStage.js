6609cd4a8e40bf2c262541ed6662fffa6c20accf
Birk Johansson <birkbjo@gmail.com>
2018-11-28 15:58:53 +0100

    Featuretype override - Remove unused options (#585)

--- src/config/field-overrides/eventProgramStage.js
+++ src/config/field-overrides/eventProgramStage.js
@@ -1,9 +1,19 @@
+import { featureTypeOverride } from './program';
+
 export default new Map([
     [
         'validationStrategy',
         {
             required: true,
             defaultValue: 'ON_COMPLETE',
+        },
+    ],
+    [
+        'featureType',
+        {
+            fieldOptions: {
+                options: featureTypeOverride,
+            },
         },
     ],
 ]);
