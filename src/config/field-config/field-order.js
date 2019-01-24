20be31ab92ba272d417daa2a61f3a0048d539af7
Hendrik de Graaf <hendrik@dhis2.org>
2019-01-24 14:24:27 +0100

    fix(indicator group set): add code field

--- src/config/field-config/field-order.js
+++ src/config/field-config/field-order.js
@@ -96,6 +96,7 @@ const fieldOrderByName = new Map([
     ]],
     ['indicatorGroupSet', [
         'name',
+        'code',
         'description',
         'compulsory',
         'indicatorGroups',
