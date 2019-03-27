a96387d9837ddefbb4984b2e058f4c90d8b780ef
Jan-Gerke Salomon <Mohammer5@users.noreply.github.com>
2019-03-27 03:25:04 +0100

    fix(program form): show names of missing inputs in snackbar

--- src/Snackbar/snack.actions.js
+++ src/Snackbar/snack.actions.js
@@ -5,12 +5,19 @@ import snackStore from './snack.store';
 const snackActions = Action.createActionsFromNames(['show', 'hide']);
 
 snackActions.show.subscribe((actionConfig) => {
-    const { message, action, autoHideDuration, onActionTouchTap, translate } = actionConfig.data;
+    const {
+        message,
+        action,
+        autoHideDuration,
+        onActionTouchTap,
+        translate,
+        variables
+    } = actionConfig.data;
 
     getD2()
         .then((d2) => {
             snackStore.setState({
-                message: translate ? d2.i18n.getTranslation(message) : message,
+                message: translate ? d2.i18n.getTranslation(message, variables) : message,
                 action,
                 autoHideDuration,
                 onActionTouchTap: onActionTouchTap || (() => {
