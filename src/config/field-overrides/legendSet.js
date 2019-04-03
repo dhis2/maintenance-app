d9c4e6a46abc927ad9de22f915b5f0c0d535a88a
ismay <ismay@ismaywolff.nl>
2019-04-03 15:32:56 +0200

    fix: validate floating point numbers correctly for legend items

--- src/config/field-overrides/legendSet.js
+++ src/config/field-overrides/legendSet.js
@@ -64,7 +64,7 @@ export default new Map([
                         const nextStart = Number(nextLegend.startValue);
 
                         // Same values are allowed, but currentEnd can not be bigger than nextStart
-                        return currentEnd < nextStart + 1;
+                        return currentEnd <= nextStart;
                     });
                 },
             },
