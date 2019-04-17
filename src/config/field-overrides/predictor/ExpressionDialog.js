4dd916e2c3ad48f830e87e92cde1aebdf6fdbcc1
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2019-04-17 11:50:37 +0200

    feat(predictor): conditionally show category combo for predictors (#672)
    * feat(predictor): conditionally show category combo for predictors
    
    * fix(type): incorrect model name
    
    * fix(predictor): improve comment clarity

---
+++ src/config/field-overrides/predictor/ExpressionDialog.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import ExpressionManager from 'd2-ui/lib/expression-manager/ExpressionManager';
+import expressionStatusStore from './expressionStatusStore'
+
+export default function ExpressionDialog({ open, handleClose, handleSaveAndClose, ...props }) {
+    const customContentStyle = {
+        width: '100%',
+        maxWidth: 'none',
+    };
+
+    const actions = [
+        <FlatButton
+            label="Cancel"
+            primary
+            onTouchTap={handleClose}
+        />,
+        <FlatButton
+            label="Submit"
+            primary
+            onTouchTap={handleSaveAndClose}
+        />,
+    ];
+
+    return (
+        <Dialog
+            open={open}
+            actions={actions}
+            contentStyle={customContentStyle}
+            style={{ padding: '1rem' }}
+            onRequestClose={handleClose}
+        >
+            <ExpressionManager
+                descriptionLabel={'description'}
+                descriptionValue={props.value ? props.value.description : ''}
+                formulaValue={props.value ? props.value.expression : ''}
+                expressionStatusStore={expressionStatusStore}
+                expressionChanged={props.indicatorExpressionChanged}
+                titleText={props.labelText}
+            />
+        </Dialog>
+    );
+}
