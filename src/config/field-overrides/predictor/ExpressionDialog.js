65fc57b7c16056c995e7b882a62ae606198ee469
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-07-01 13:35:05 +0200

    feat(predictor): use predictor endpoint for expression validation

--- src/config/field-overrides/predictor/ExpressionDialog.js
+++ src/config/field-overrides/predictor/ExpressionDialog.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import ExpressionManager from 'd2-ui/lib/expression-manager/ExpressionManager';
@@ -32,6 +33,7 @@ export default function ExpressionDialog({ open, handleClose, handleSaveAndClose
             onRequestClose={handleClose}
         >
             <ExpressionManager
+                validateExpression={props.validateExpression}
                 descriptionLabel={'description'}
                 descriptionValue={props.value ? props.value.description : ''}
                 formulaValue={props.value ? props.value.expression : ''}
@@ -41,4 +43,8 @@ export default function ExpressionDialog({ open, handleClose, handleSaveAndClose
             />
         </Dialog>
     );
+}
+
+ExpressionDialog.propTypes = {
+    validateExpression: PropTypes.func,
 }
