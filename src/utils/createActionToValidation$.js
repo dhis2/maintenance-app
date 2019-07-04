ae55e1f11de19b2d441ec43d25a04d7d92e1ac51
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-07-04 14:51:46 +0200

    fix(expression validation): translate message when endpoint responds with 500

--- src/utils/createActionToValidation$.js
+++ src/utils/createActionToValidation$.js
@@ -6,7 +6,10 @@ export const createActionToValidation$ = url => ({data}) => {
     const request = getD2()
         .then(d2 => d2.Api.getApi())
         .then(api => api.post(url, data, options))
-        .catch(e => ({ ...e, message: 'Expression is empty' }))
+        .catch(e => getD2().then(d2 => ({
+            ...e,
+            message: d2.i18n.getTranslation('Expression is empty'),
+        })))
 
     return Observable.fromPromise(request);
 };
