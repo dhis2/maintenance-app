2c031dd3adaaa994e2b1af1fabc9d90b37528ae3
Birk Johansson <birk@dhis2.org>
2019-10-22 17:51:51 +0200

    fix(programstage): fetch attributeValues for programStage

--- src/EditModel/event-program/epics.js
+++ src/EditModel/event-program/epics.js
@@ -158,7 +158,7 @@ function loadEventProgramMetadataByProgramId(programPayload) {
     const queryParams = {
         fields: ':owner,displayName',
         'programs:filter': `id:eq:${programId}`,
-        'programs:fields': `${programFields},programStages[:owner,user[id,name],displayName,programStageDataElements[:owner,renderType,dataElement[id,displayName,valueType,optionSet,domainType]],notificationTemplates[:owner,displayName],dataEntryForm[:owner],programStageSections[:owner,displayName,dataElements[id,displayName]]]`,
+        'programs:fields': `${programFields},programStages[:owner,user[id,name],displayName,attributeValues[:all,attribute[id,name,displayName]],programStageDataElements[:owner,renderType,dataElement[id,displayName,valueType,optionSet,domainType]],notificationTemplates[:owner,displayName],dataEntryForm[:owner],programStageSections[:owner,displayName,dataElements[id,displayName]]]`,
         'dataElements:fields': 'id,displayName,valueType,optionSet',
         'dataElements:filter': 'domainType:eq:TRACKER',
         'trackedEntityAttributes:fields': 'id,displayName,valueType,optionSet,unique'
