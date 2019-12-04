435d9baf4394e516c67ee563f4b167480cbb52e1
Hendrik de Graaf <hendrik@dhis2.org>
2019-12-04 10:24:13 +0100

    fix(predictor): show missing-value-strategy dropdown (DHIS2-7147 - v31)

--- src/config/field-overrides/predictor/ExpressionDialog.js
+++ src/config/field-overrides/predictor/ExpressionDialog.js
@@ -3,6 +3,7 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import ExpressionManager from 'd2-ui/lib/expression-manager/ExpressionManager';
 import expressionStatusStore from './expressionStatusStore'
+import MissingValueStrategy from '../validation-rules/MissingValueStrategy';
 
 export default function ExpressionDialog({ open, handleClose, handleSaveAndClose, ...props }) {
     const customContentStyle = {
@@ -23,6 +24,10 @@ export default function ExpressionDialog({ open, handleClose, handleSaveAndClose
         />,
     ];
 
+    // Only display the MissingValueStrategy for GENERATOR, not SAMPLE SKIP TEST
+    // see https://jira.dhis2.org/browse/DHIS2-7147?focusedCommentId=29805&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-29805
+    const showMissingValueStrategy = props.referenceProperty === 'generator';
+
     return (
         <Dialog
             open={open}
@@ -31,6 +36,12 @@ export default function ExpressionDialog({ open, handleClose, handleSaveAndClose
             style={{ padding: '1rem' }}
             onRequestClose={handleClose}
         >
+            {showMissingValueStrategy && (
+                <MissingValueStrategy
+                    value={props.value ? props.value.missingValueStrategy : ''}
+                    onChange={props.missingValueStrategyChanged}
+                />
+            )}
             <ExpressionManager
                 descriptionLabel={'description'}
                 descriptionValue={props.value ? props.value.description : ''}
