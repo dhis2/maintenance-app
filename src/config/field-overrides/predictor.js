07605f80a84353749cd6d48d03c4954e4775d150
Birk Johansson <birkbjo@gmail.com>
2020-02-17 17:07:32 +0100

    fix(predictors): set unique: false to prevent async validation (#1015)

--- src/config/field-overrides/predictor.js
+++ src/config/field-overrides/predictor.js
@@ -34,6 +34,9 @@ export default new Map([
                 message: 'expression_is_required',
             },
         ],
+        // Override unique, it was set to true on the backend for an unkown reason
+        // see https://jira.dhis2.org/browse/DHIS2-8311
+        unique: false
     }],
     ['sampleSkipTest', {
         component: PredictorExpressionField,
@@ -47,6 +50,8 @@ export default new Map([
                 message: 'expression_is_required',
             },
         ],
+        // see https://jira.dhis2.org/browse/DHIS2-8311
+        unique: false
     }],
     ['outputCombo', {
         component: DataElementCategoryOptionCombo,
