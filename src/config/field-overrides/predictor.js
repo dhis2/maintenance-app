8654adc47f6c7057c50b19a98dde905629737615
Birk Johansson <birkbjo@gmail.com>
2020-02-18 13:58:52 +0100

    fix(predictors): set unique: false to prevent async validation (#1018)
    docs(predictor): add comment for uniqueness

--- src/config/field-overrides/predictor.js
+++ src/config/field-overrides/predictor.js
@@ -3,44 +3,40 @@ import ExpressionField from './predictor/ExpressionField';
 import DataElementCategoryOptionCombo from './predictor/DataElementCategoryOptionCombo';
 
 export default new Map([
-    [
-        'periodType',
-        {
-            component: PeriodTypeDropDown,
-        },
-    ],
-    [
-        'generator',
-        {
-            component: ExpressionField,
-            validators: [
-                {
-                    validator: value => Boolean(value && value.description),
-                    message: 'description_is_required',
-                },
-                {
-                    validator: value => Boolean(value && value.expression),
-                    message: 'expression_is_required',
-                },
-            ],
-        },
-    ],
-    [
-        'sampleSkipTest',
-        {
-            component: ExpressionField,
-            validators: [
-                {
-                    validator: value => Boolean(value && value.description),
-                    message: 'description_is_required',
-                },
-                {
-                    validator: value => Boolean(value && value.expression),
-                    message: 'expression_is_required',
-                },
-            ],
-        },
-    ],
+    ['periodType', {
+        component: PeriodTypeDropDown,
+    }],
+    ['generator', {
+        component: ExpressionField,
+        validators: [
+            {
+                validator: value => Boolean(value && value.description),
+                message: 'description_is_required',
+            },
+            {
+                validator: value => Boolean(value && value.expression),
+                message: 'expression_is_required',
+            },
+        ],
+        // Override unique, it was set to true on the backend for an unkown reason
+        // see https://jira.dhis2.org/browse/DHIS2-8311
+        unique: false
+    }],
+    ['sampleSkipTest', {
+        component: ExpressionField,
+        validators: [
+            {
+                validator: value => Boolean(value && value.description),
+                message: 'description_is_required',
+            },
+            {
+                validator: value => Boolean(value && value.expression),
+                message: 'expression_is_required',
+            },
+        ],
+        // See https://jira.dhis2.org/browse/DHIS2-8311
+        unique: false
+    }],
     [
         'outputCombo',
         {
