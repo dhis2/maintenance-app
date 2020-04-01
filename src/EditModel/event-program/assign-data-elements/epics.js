7cd3432f914336d8a39e52c60b9dc9273d30a703
Birk Johansson <birkbjo@gmail.com>
2020-04-01 12:33:06 +0200

    fix(data-entry-form): remove DataElement from sections when removed from stage (#1091)
    fix: extra check for programStageSection
    
    refactor: rename variable

--- src/EditModel/event-program/assign-data-elements/epics.js
+++ src/EditModel/event-program/assign-data-elements/epics.js
@@ -19,6 +19,7 @@ import {
     __,
 } from 'lodash/fp';
 import { generateUid } from 'd2/lib/uid';
+import { getStageSectionsById } from "../tracker-program/program-stages/selectors";
 
 // getProgramStageToModify :: String -> ProgramStage[] -> ProgramStage
 export const getProgramStageToModify = (
@@ -94,10 +95,12 @@ const removeDataElementFromStage = store => action$ =>
     action$
         .ofType(PROGRAM_STAGE_DATA_ELEMENTS_REMOVE)
         .map(action => {
+            const state = store.getState();
             const programStageToEdit = getProgramStageByIdFromAction(
                 store,
                 action
             );
+            const programStageToEditId = programStageToEdit.id;
 
             const programStageDataElements = getOr(
                 [],
@@ -117,6 +120,34 @@ const removeDataElementFromStage = store => action$ =>
             programStageToEdit.programStageDataElements = removeDataElements(
                 programStageDataElements
             );
+
+            // Remove DataElements from any sections that contains the removed DataElements
+            let programStageSections = getStageSectionsById(
+                state,
+                programStageToEditId
+            );
+
+            if (
+                programStageSections &&
+                Object.keys(programStageSections).length > 0
+            ) {
+                state.programStageSections[
+                    programStageToEditId
+                ] = programStageSections.map(section => {
+                    const updatedDataElements = section.dataElements.filter(
+                        element => !dataElementIdsToRemove.includes(element.id)
+                    );
+
+                    if (
+                        section.dataElements.length !==
+                        updatedDataElements.length
+                    ) {
+                        section.dataElements = updatedDataElements;
+                    }
+                    return section;
+                });
+            }
+
             const programStages = getOr([], 'programStages', store.getState());
             store.setState({
                 ...store.getState(),
