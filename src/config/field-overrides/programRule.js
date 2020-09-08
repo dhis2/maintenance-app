70f1e0f75fa3488be44a07f5c5bb9e9445da98a4
Birk Johansson <birk@dhis2.org>
2020-09-08 13:55:16 +0200

    fix(programrule): use programId for validation (#1369)
    * fix(programrule): check for status so PRA-dialog can be opened
    
    * fix(programrule): fix validation when creating new programrule
    
    * fix(programrule): add validation detail message

--- src/config/field-overrides/programRule.js
+++ src/config/field-overrides/programRule.js
@@ -15,33 +15,50 @@ import { ExpressionStatus } from './program-indicator/ExpressionStatusIcon';
 import { AnalyticsPeriodBoundaries } from './program-indicator/AnalyticsPeriodBoundaries';
 import createExpressionValidator from './program-indicator/createExpressionValidator';
 
+const setValidatorSubscription = (context, subFunc, programId) => {
+    const path = `programRules/condition/description?programId=${
+        programId
+    }`;
+    const { status$, validate } = createExpressionValidator(path, subFunc);
+    context.validatorStatusSubscription = status$.subscribe(subFunc)
+    context.validate = validate
+    context.props.setValidatorProgramId(programId)
+    return { status$, validate }
+};
+
 const enhance = compose(
     getContext({ d2: PropTypes.object }),
     withState('status', 'setStatus', ({ d2 }) => ({
         status: ExpressionStatus.PENDING,
         message: d2.i18n.getTranslation('checking_expression_status'),
     })),
+    withState('validatorProgramId', 'setValidatorProgramId', () => undefined),
     lifecycle({
         componentDidMount() {
             const { setStatus, value } = this.props;
-            const path = `programRules/condition/description?programRule=${this.props.model.id}`
-            const { status$, validate } = createExpressionValidator(path, setStatus);
-
-            this.validate = validate;
-
-            this.validatorStatusSubscription = status$.subscribe(setStatus);
-
+            if(!this.props.model || !this.props.model.program || !this.props.model.program.id) {
+                // this only happens when a new rule is created
+                setStatus(null) // prevent status from being shown when program is not selected
+                return
+            }
+            setValidatorSubscription(this, setStatus, this.props.model.program.id)
             this.validate(value);
         },
 
         componentWillReceiveProps(newProps) {
-            if (newProps.value !== this.props.value) {
+            // Need to create a new validator when program is changed, as the validation depends on it
+            if (newProps.model.program && (!this.props.validatorProgramId || newProps.model.program.id !== this.props.validatorProgramId)) {
+                this.validatorStatusSubscription && this.validatorStatusSubscription.unsubscribe();
+                setValidatorSubscription(this, this.props.setStatus, newProps.model.program.id);
+                this.validate(newProps.value);
+            }
+            else if (newProps.value !== this.props.value) {
                 this.validate(newProps.value);
             }
         },
 
         componentWillUnmount() {
-            this.validatorStatusSubscription.unsubscribe();
+            this.validatorStatusSubscription && this.validatorStatusSubscription.unsubscribe();
         },
     }),
 );
