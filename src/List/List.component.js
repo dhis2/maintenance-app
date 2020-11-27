97adb15cfd2fe671f211a003caded92b7d552895
Birk Johansson <birk@dhis2.org>
2020-11-27 16:50:35 +0100

    feat(relationshiptype): make toName and fromName translateable

--- src/List/List.component.js
+++ src/List/List.component.js
@@ -116,6 +116,8 @@ export function getTranslatablePropertiesForModelType(modelType) {
         return ['name'];
     case 'program':
         return defaultTranslatableProperties.concat(['description'])
+    case 'relationshipType':
+        return defaultTranslatableProperties.concat(['fromName', 'toName'])
     default:
         break;
     }
