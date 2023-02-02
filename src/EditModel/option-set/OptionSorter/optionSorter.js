0de0bb2c3130ac978e4989e2e89a3419a4a6830f
Rithvik Reddy Koripelli <rithvikreddykoripelli@gmail.com>
2023-02-02 22:07:45 +0530

    fix: sorting options in natural sort order [DHIS2-13714]

--- src/EditModel/option-set/OptionSorter/optionSorter.js
+++ src/EditModel/option-set/OptionSorter/optionSorter.js
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 export default function optionSorter(options, sortProperty, sortOrder = 'ASC') {
     const sortedOptions = options.sort((left, right) =>
         (left[sortProperty] || '')
-            .localeCompare(right[sortProperty]));
+            .localeCompare(right[sortProperty],undefined, {numeric: true}));
 
     return Observable
         .of(sortOrder === 'ASC'
