f46f77d74b671815a7cb8a5bc64c18054de781a5
Birk Johansson <birk@dhis2.org>
2024-06-05 16:20:31 +0200

    fix(date): remove z from converted string

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,11 +1,11 @@
 /**
- * Formats the date to an ISO string where the timezone of date is ignored before conversion to UTC.
+ * Formats the date to an ISO-formatted string (without z) where the timezone of date is ignored before conversion to UTC.
  * 
  * This is useful for date-selectors where we get information with timezone information,
  * but we are just interested in the date itself, and don't want the time to be adjusted when converting to UTC.
- * @param {Date} date - the date to convert to an UTC ISO string
+ * @param {Date} date - the date to convert to an ISO-formatted string without timezone information
  * @returns date formatted as ISO string without timezone information
  */ 
 export const getISOFormatLocalTimestamp = date =>
     new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-        .toISOString()
+        .toISOString().substring(0,23)
