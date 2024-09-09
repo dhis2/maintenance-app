9b01d21ed03709a5ff7d78bd8c49a2a553487534
Birk Johansson <birk@dhis2.org>
2024-09-09 13:53:55 +0200

    fix(categoryCombo): make categories required (#2913)

--- src/config/field-overrides/categoryCombo.js
+++ src/config/field-overrides/categoryCombo.js
@@ -3,6 +3,13 @@ import MultiSelect from '../../forms/form-fields/multi-select';
 
 export default new Map([
     ['categories', {
+        required: true,
+        validators: [{ // regular isRequired does not check for empty collection or array
+            validator(value, formState) {
+                return value && value.size > 0;
+            },
+           message: 'categories_cannot_be_empty',
+        }],
         component: (props) => {
             let queryFilter;
 
