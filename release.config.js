fb13ac367f9a07462dfcbb85a35cd93d6c6c3b5e
Flaminia Cavallo <flaminia@dhis2.org>
2026-03-16 12:29:18 +0100

    fix: call semantic release directly

--- release.config.js
+++ release.config.js
@@ -3,6 +3,9 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        ['@semantic-release/npm', {
+            npmPublish: false,
+        }],
         '@semantic-release/github',
     ],
 }
